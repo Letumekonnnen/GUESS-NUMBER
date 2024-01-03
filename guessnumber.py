46c55bad09718df46692b516073beb4ae21ce86b
Letu Mekonnen <letinamekonnen6@gmail.com>
2024-01-04 02:22:05 +0300

    edit guess number

--- guessnumber.py
+++ guessnumber.py
@@ -1,35 +1,36 @@
 import random
 
-top_of_range = input("Type a number: ")
+def get_user_input():
+    while True:
+        user_input = input("Type a number: ")
+        
+        if user_input.isdigit():
+            return int(user_input)
+        else:
+            print("Please type a number next time.")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-
+def play_guessing_game():
+    top_of_range = get_user_input()
+    
     if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
-        quit()
-else:
-    print('Please type a number next time.')
-    quit()
+        print("Please type a number larger than 0 next time.")
+        return
+    
+    random_number = random.randint(0, top_of_range)
+    guesses = 0
+    
+    while True:
+        guesses += 1
+        user_guess = get_user_input()
+        
+        if user_guess == random_number:
+            print("You got it!")
+            break
+        elif user_guess > random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
+    
+    print("You got it in", guesses, "guesses")
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
-
-while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print('Please type a number next time.')
-        continue
-
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("You were above the number!")
-    else:
-        print("You were below the number!")
-
-print("You got it in", guesses, "guesses")
+play_guessing_game()
